5eab8b682d6af62358d1dfc58fff3f49c4ce9a46
GwakSeungju <gwak1016@gmail.com>
2024-02-10 15:55:14 +0900

    Add: 카테고리 랭킹 컴포넌트 추가

--- src/components/CategoryRanking.js
+++ src/components/CategoryRanking.js
@@ -1,41 +1,10 @@
-import { useEffect, useState } from 'react';
-import useStore from '../store/store';
 import { Link } from 'react-router-dom';
+import useStore from '../store/store';
 
 export default function CategoryRanking() {
-    const { shoppingData, items, addVisitedProduct } = useStore((state) => state);
-    const productList = ['상의', '아우터', '바지', '스커트', '가방', '신발', '시계', '모자', '스포츠', '안경'];
-    const [currentCategory, setCurrentCategory] = useState(0);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentCategory((prev) => (prev + 1) % productList.length);
-        }, 8000);
-
-        return () => clearInterval(intervalId);
-    }, []);
-
-    useEffect(() => {
-        shoppingData(productList[currentCategory]);
-    }, [currentCategory]);
-
+    const { items, addVisitedProduct } = useStore((state) => state);
     return (
-        <div id="rankingContainer">
-            <div id="productListContainer">
-                <div>상품</div>
-                {productList.map((item, index) => (
-                    <button
-                        key={index}
-                        className={'productBtn' + (index === currentCategory ? ' active' : '')}
-                        onClick={() => {
-                            setCurrentCategory(index);
-                            shoppingData(item);
-                        }}
-                    >
-                        {item}
-                    </button>
-                ))}
-            </div>
+        <>
             <div id="searchBoxContainer">
                 {items &&
                     items.map((item, index) => (
@@ -59,6 +28,6 @@ export default function CategoryRanking() {
                         </div>
                     ))}
             </div>
-        </div>
+        </>
     );
 }
