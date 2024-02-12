6632dc5d874c7cbfc13ce5e21573ad90579289fd
GwakSeungju <gwak1016@gmail.com>
2024-02-12 14:41:43 +0900

    Feat: 방문했던 상품 클릭 시 페이지 이동 기능 추가

--- src/components/VisitedProduct.js
+++ src/components/VisitedProduct.js
@@ -1,14 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import useStore from '../store/store';
 
 export default function VisitedProduct() {
     const { visitedProduct, clearVisitedProduct } = useStore((state) => state);
+    const navigate = useNavigate();
     return (
         <div id="visitedContainer">
             <div id="visitedHead">visited</div>
             {visitedProduct.length !== 0
                 ? visitedProduct.slice(0, 5).map((item, index) => (
                       <span key={index}>
-                          <img id="visitedProductImage" src={item.image} alt={item.mallName}></img>
+                          <img
+                              id="visitedProductImage"
+                              src={item.image}
+                              alt={item.mallName}
+                              className="btn"
+                              onClick={() => {
+                                  navigate(`/ProductPage/${item.productId}/${index + 1}`, { state: { item } });
+                              }}
+                          ></img>
                       </span>
                   ))
                 : null}
